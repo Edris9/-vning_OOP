a41d104e18f17c93e2e40755b1471ccfc1f5923b
Edris9 <Edriskohestani1010@gmail.com>
2025-09-22 01:14:37 +0200

    add some changes in main and and add inheritence file there user can make another objek which it inherice from parents class please check it  call methods in main

--- Program.cs
+++ Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hej välkommen till banken");
+            Console.WriteLine("Tryck 1 för att testa encapsulation:\nTryck 2 för att testa inheritance:");
+
+            string val = Console.ReadLine();
+
+            if (val == "1")
+            {
+                encapsulation_account_nnr account = new encapsulation_account_nnr();
+                account.GetPersonID(1234567890);
+                account.Deposit(1000);
+                account.Withdraw(500);
+                Console.WriteLine("Ditt saldo är: " + account.GetBalance());
+            }
+            else if (val == "2")
+            {
+                inheritance student = new inheritance();
+                Console.WriteLine("Ange namn:");
+                student.Name = Console.ReadLine();
+                Console.WriteLine("Ange ålder:");
+                student.Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ange adress:");
+                student.Address = Console.ReadLine();
+                student.Askaboutnewobject();
+            }
         }
     }
 }
